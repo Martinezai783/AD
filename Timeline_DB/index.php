ae88b95bf7f64aecc4fe24bf8a9a2029ba02fdfd
Lucasmggg <113194673+Lucasmggg@users.noreply.github.com>
2023-03-02 16:18:20 +0100

    Cambios Lucas

---
+++ Timeline_DB/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">   
+</head>
+<body>
+    <div class="container" style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center; height: 100vh;">
+        <a href="./jugar/dummy.html" style="width: 40%; height: 40%; margin: 10px;">
+            <button type="button" class="btn btn-secondary btn-lg" style="width: 100%; height: 100%; font-size: 40px">Jugar</button>
+        </a>
+        <a href="./admin/admin.html" style="width: 40%; height: 40%; margin: 10px;">
+            <button type="button" class="btn btn-secondary btn-lg" style="width: 100%; height: 100%; font-size: 40px">Admin</button>
+        </a>
+    </div>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>    
+</body>
+</html>
